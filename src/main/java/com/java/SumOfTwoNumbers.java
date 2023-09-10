4122f374ccffdc466ca105528b81c0d7fb29b4ec
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-10 21:52:42 +0300

    SumOfTwoNumbers

---
+++ src/main/java/com/java/SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package com.java;
+
+
+public class SumOfTwoNumbers {
+	
+	public static void main(String[] args) {
+		
+		int num1= 6;
+		int num2 = 10;
+		int sum = num1+num2;
+		System.out.println("Sum of Two Nymbers :"+ sum);
+		
+	}
+}
