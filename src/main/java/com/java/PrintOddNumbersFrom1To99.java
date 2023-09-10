e4a252ceb2bec6f5e199459dcfee3d402b219aa3
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-10 21:52:15 +0300

    PrintOddNumbersFrom1To99

---
+++ src/main/java/com/java/PrintOddNumbersFrom1To99.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class PrintOddNumbersFrom1To99 {
+
+	public static void main(String[] args) {
+		
+		int number = 1;
+		
+		for(int i = 1 ;i<=99;i+=2) {
+			System.out.println(i);
+		}
+
+
+	}
+
+}
