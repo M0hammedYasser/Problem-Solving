c758eaed6fd76073b6afa1b9c9439b813cde9b86
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-10 21:50:34 +0300

    Watermelon

---
+++ src/main/java/com/java/Watermelon.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Watermelon {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int num = input.nextInt();
+		
+		if(num%2 ==0 &&  num!=2  )
+		{
+			System.out.println("YES");
+		}
+		else {
+			System.out.println("No");
+		}
+		
+
+	}
+
+}
