826a261551507f2180e8dc1b20a8f0b8490d4830
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-10 21:48:46 +0300

    ConvertAStringToAnInteger

---
+++ src/main/java/com/java/ConvertAStringToAnInteger.java
@@ -0,0 +1,19 @@
+package com.java;
+
+import java.util.Scanner;
+
+
+public class ConvertAStringToAnInteger {
+
+	public static void main(String[] args) {
+		
+	Scanner input = new Scanner(System.in);
+
+	String string =input.next();
+	
+	int result = Integer.parseInt(string);
+	
+
+	}
+
+}
