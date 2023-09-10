9deb8e4e67062501294878bcc8e685d02deb3e48
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-10 21:49:34 +0300

    DisplayTheCurrentDateAndTime

---
+++ src/main/java/com/java/DisplayTheCurrentDateAndTime.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Date;
+
+public class DisplayTheCurrentDateAndTime {
+
+	public static void main(String[] args) {
+		
+		Date date= new Date();
+		
+		System.out.println(date.clone());
+	}
+
+}
