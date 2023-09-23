51a71027b058fdc2aef9c871e60b38deeb93891d
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-23 22:24:00 +0300

    Data-Types

---
+++ src/main/java/com/java/DataTypes.java
@@ -0,0 +1,34 @@
+package com.java;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        //Numbers Data Type
+        byte num1 = 127;                  //from -128 to 127
+        short num2 =  32767;             //from -32,768 to 32,767
+        int num3 =  2147483647;         //from -2,147,483,648 to 2,147,483,647
+        long num4 = 922337203;         //from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        float num5 = 12465.5f;        // 6 to 7 decimal digits
+        double num6 = 1316164.2634;  // 15 decimal digits
+
+        //Text Data Type
+        String name = "muhammed yasser"; //
+        char letter = 'A';
+
+        //Boolean Data Type
+        boolean isHere = true;
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
