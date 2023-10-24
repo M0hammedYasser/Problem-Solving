6a15c6437fa1f8dc67c1065d944b588b36ddf5be
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-10-24 21:30:51 +0300

    Elephant

---
+++ src/main/java/com/java/Elephant.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Elephant {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double x= input.nextDouble();
+        System.out.println((int)Math.ceil( x /5));
+
+    }
+}
