1a90378b736028e051c0c219efc5dbd0ed9d0f47
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-11-01 21:33:53 +0200

    HitTheLottery

---
+++ src/main/java/com/java/HitTheLottery.java
@@ -0,0 +1,31 @@
+package com.java.current;
+
+import java.util.Scanner;
+
+public class HitTheLottery {
+
+    public static void main(String[] args) {
+            long numOfDollars = new Scanner(System.in).nextInt();
+            System.out.println(numOfPaper(numOfDollars));
+    }
+
+    public static long numOfPaper(long num) {
+        long count;
+        count = num / 100;
+
+        num %= 100;
+        count += num / 20;
+
+        num %= 20;
+        count += num / 10;
+
+        num %= 10;
+        count += num / 5;
+
+        num %= 5;
+        count += num;
+
+        return count;
+
+    }
+}
