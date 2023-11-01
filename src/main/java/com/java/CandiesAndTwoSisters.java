1be9313eeae467a14a867e2c7427abb00097bc87
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-11-01 21:32:26 +0200

    CandiesAndTwoSisters

---
+++ src/main/java/com/java/CandiesAndTwoSisters.java
@@ -0,0 +1,28 @@
+package com.java.current;
+
+import java.util.Scanner;
+
+public class CandiesAndTwoSisters {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        for (int i = 0; i < num; i++) {
+            int testCase = input.nextInt();
+            System.out.println(numOfWay(testCase));
+        }
+    }
+
+    public static int numOfWay(int num) {
+        if (num <= 2)
+            return 0;
+        else {
+            if (num % 2 != 0)
+                return num / 2;
+            else
+                return (num / 2) - 1;
+        }
+    }
+
+}
+
