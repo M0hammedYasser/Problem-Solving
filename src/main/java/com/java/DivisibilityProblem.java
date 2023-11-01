f14e6bcbbde41066d79910199983f7079b620444
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-11-01 21:32:59 +0200

    DivisibilityProblem

---
+++ src/main/java/com/java/DivisibilityProblem.java
@@ -0,0 +1,22 @@
+package com.java.current;
+
+import java.util.Scanner;
+
+public class DivisibilityProblem {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numOfTestCasing = input.nextInt();
+        double count = 0;
+
+        for (int i = 0; i < numOfTestCasing; i++) {
+            double A = input.nextDouble();
+            double B = input.nextDouble();
+
+            count = (Math.ceil(A / B)) * B;
+            System.out.println((int)(count - A));
+        }
+
+    }
+}
+
