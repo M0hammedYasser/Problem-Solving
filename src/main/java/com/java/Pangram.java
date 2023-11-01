ec24693d28f524e42751acb151b4d7ab2e6e9eda
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-11-01 21:35:35 +0200

    Pangram

---
+++ src/main/java/com/java/Pangram.java
@@ -0,0 +1,27 @@
+package com.java.current;
+
+import java.util.*;
+
+public class Pangram {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int length = input.nextInt();
+        String word = input.next();
+        System.out.println(checkWord(word));
+
+
+    }
+    public static String checkWord(String name) {
+        char[] words = name.toCharArray();
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            set.add(Character.toUpperCase(words[i]));
+        }
+        if (set.size() >= 26)
+            return "YES";
+        else
+            return "NO";
+    }
+}
