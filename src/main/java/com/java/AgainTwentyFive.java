fad4e87871d403c4678d6a2cff88635496a9bd7e
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-11-13 20:27:21 +0200

    AgainTwentyFive

---
+++ src/main/java/com/java/AgainTwentyFive.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        long power = input.nextLong();
+        System.out.println(25);
+
+
+    }
+}
