343f6c28ea768c2013bf3c1c39e9962e3e1d8ad3
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-12-26 23:13:51 +0200

    EhabAndGcd

---
+++ src/main/java/com/java/EhabAndGcd.java
@@ -0,0 +1,17 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class EhabAndGcd {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+
+		for (int i = 0; i < t; i++) {
+			long x = scanner.nextLong();
+			System.out.println("1" + " " +( x -1));
+		}
+	}
+
+}
