0cb1522622c3d9b68755b5cf87db64160e51b98f
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2024-01-08 23:48:07 +0200

    OmkarAndCompletion

---
+++ src/main/java/com/java/OmkarAndCompletion.java
@@ -0,0 +1,20 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class OmkarAndCompletion {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int t = input.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = input.nextInt();
+			for (int j = 1; j <= n; j++) {
+				System.out.print( 1 + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
